fd66d77d48cf0225b8b72e658ce9422c05c7ec50
Kirk Whetton <kirkwhetton@Kirks-MacBook-Pro.local>
2024-03-25 00:33:32 +0000

    ci: added shutdown AA runbook

---
+++ Azure Automation Runbooks/ShutdownVM.ps1
@@ -0,0 +1,35 @@
+Param (
+  [Parameter (Mandatory= $false)]
+  [Array] $VMNames,
+
+  [Parameter (Mandatory= $true)]
+  [String] $Subscription = "MySubscriptionName",
+
+  [Parameter (Mandatory= $true)]
+  [String] $ResourceGroup = "MyResourceGroup"
+)
+
+# Configure Login
+Disable-AzContextAutoSave -Scope Process
+$AzureContext = (Connect-AzAccount -Identity).context
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+
+#-------------------------------------------- Start Script --------------------------------------------#
+
+# Shutdown virtual machines that are running with the shutdown tag enabled or with an array from user input.
+
+if($VMNames){
+    foreach ($VM in $VMNames){
+        Stop-AzVM -ResourceGroupName $ResourceGroup -Name $VM -Force -NoWait
+    }
+}
+else { 
+    $VMs = Get-AzVM -ResourceGroupName $ResourceGroup | Where-Object {$_.Tags['Shutdown'] -eq 'true'}
+    foreach ($VM in $VMs) {
+        $Status = Get-AzVM -ResourceGroupName $ResourceGroup -Name $VM.Name -Status
+        if ($Status.Statuses[1].Code -eq "Powerstate/running"){
+            Stop-AzVM -ResourceGroupName $ResourceGroup -Name $VM.Name -Force -NoWait
+        }
+    }
+}
+
